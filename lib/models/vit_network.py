aa8bb7484236762f75f141c0f121ea9c8d8fcf1f
Your Name <you@example.com>
2023-04-10 12:40:17 +0200

    non_striding conv

--- lib/models/vit_network.py
+++ lib/models/vit_network.py
@@ -24,12 +24,13 @@ class VitFeatureExtractor(BaseFeatureExtractor):
             # kernel=5: out=22^2
             # kernel=7: out=26^2
             # kernel=9: out=30^2
+            out_size = 18
             self.projector = nn.Sequential(nn.ReLU(inplace=False),
-                                           nn.ConvTranspose2d(384, 256, kernel_size=3, stride=1, padding=0, bias=False),
-                                           nn.ReLU(inplace=False),
-                                           nn.ConvTranspose2d(256, 128, kernel_size=3, stride=1, padding=0, bias=False),
-                                           nn.ReLU(inplace=False),
-                                           nn.Conv2d(128, config_model.descriptor_size, kernel_size=1, stride=1,
+                                           #nn.ConvTranspose2d(384, 256, kernel_size=3, stride=1, padding=0, bias=False),
+                                           #nn.ReLU(inplace=False),
+                                           #nn.ConvTranspose2d(256, 128, kernel_size=3, stride=1, padding=0, bias=False),
+                                           #nn.ReLU(inplace=False),
+                                           nn.Conv2d(384, config_model.descriptor_size, kernel_size=out_size, stride=out_size,
                                                      padding=0, bias=False))
 
     def forward(self, x):
