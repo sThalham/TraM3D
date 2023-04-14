28e3bc855b04d3340b010e050828d3d6321a084a
Your Name <you@example.com>
2023-04-14 16:46:43 +0200

    projector and predictor head

--- lib/models/vit.py
+++ lib/models/vit.py
@@ -87,6 +87,36 @@ class DropPath(nn.Module):
         return drop_path(x, self.drop_prob, self.training)
 
 
+class Mlp_projection(nn.Module):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.bn1 = nn.BatchNorm2d(hidden_features)
+        self.act1 = act_layer()
+        self.fc2 = nn.Linear(hidden_features, hidden_features)
+        self.bn2 = nn.BatchNorm2d(hidden_features)
+        self.act2 = act_layer()
+        self.fc3 = nn.Linear(hidden_features, out_features)
+        self.bn3 = nn.BatchNorm2d(out_features)
+        self.drop = nn.Dropout(drop)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.bn1(x)
+        x = self.act1(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.bn2(x)
+        x = self.act2(x)
+        x = self.drop(x)
+        x = self.fc3(x)
+        x = self.bn3(x)
+        x = self.drop(x)
+        return x
+
+
 class Mlp_prediction(nn.Module):
     def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
         super().__init__()
@@ -230,11 +260,15 @@ class VisionTransformer(nn.Module):
         #self.head = Block(dim=128, num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
         #    drop=drop_rate, attn_drop=attn_drop_rate, drop_path=0.0, norm_layer=norm_layer)
 
-        mlp_hidden_dim = int(embed_dim * 4.0)
-        self.head = Mlp_prediction(in_features=embed_dim, hidden_features=mlp_hidden_dim, out_features=num_classes, act_layer=nn.GELU, drop=0.0)
-
-        self.head = nn.Linear(embed_dim, num_classes)
+        head_activation = nn.GELU
+        projector_hidden_dim = int(embed_dim * 4.0)
+        predictor_hidden_dim = int(num_classes * 4.0)
+        self.projector = Mlp_projection(in_features=embed_dim, hidden_features=projector_hidden_dim,
+                                        out_features=num_classes, act_layer=head_activation, drop=0.0)
+        self.predictor = Mlp_prediction(in_features=num_classes, hidden_features=predictor_hidden_dim, out_features=num_classes, act_layer=head_activation, drop=0.0)
         self.norm_head = norm_layer(num_classes)
+        #self.head = nn.Linear(embed_dim, num_classes)
+        #self.norm_pred = norm_layer(num_classes)
 
         trunc_normal_(self.pos_embed, std=.02)
         trunc_normal_(self.cls_token, std=.02)
@@ -295,9 +329,10 @@ class VisionTransformer(nn.Module):
         if self.use_avg_pooling_and_fc:
             return x[:, 0]
         else:
-            # another head for projecting to lower-sized feature vector
-            x = self.head(x)
-            #x = self.norm_head(x)
+            x = self.projector(x)
+            x = self.norm_head(x)
+            x = self.predictor(x)
+            x = self.norm_head(x)
             #####
             x = x[:, 1:, :].permute(0, 2, 1)
             B, F, _ = x.size()
